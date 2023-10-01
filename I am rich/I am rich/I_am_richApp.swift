b38dd7cb81e595e0d0ad0ddfc60925d3d3de8310
michelle <lune111200@gmail.com>
2023-10-01 09:33:40 +0900

    SwiftUI Basic

---
+++ I am rich/I am rich/I_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_richApp.swift
+//  I am rich
+//
+//  Created by SeungYeon Yoo on 2023/10/01.
+//
+
+import SwiftUI
+
+@main
+struct I_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
